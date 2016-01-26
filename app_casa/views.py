e83eba503cafc8ad2dc5045b07b42fdf9e5f8cb8
eucandre <eucandre@gmail.com>
2016-01-26 17:59:27 -0200

    resolvendo os relatorios em graficos

--- app_casa/views.py
+++ app_casa/views.py
@@ -7,40 +7,11 @@ def pagina_inicial(request):
 
     tamanho_receitas = len(entradas.objects.all())
     tamanho_despesas = len(saidas.objects.all())
-
-    obj_receitas=entradas.objects.get(pk=tamanho_receitas)
-    obj_despesas=entradas.objects.get(pk=tamanho_despesas)
-
-    nome_gasto = obj_despesas.nome_saida
-    valor = obj_despesas.valor_saida
-    lista_produtos_valorizados = []
-    maior = 0
-    maior1 = 0
-    maior2 = 0
-    maior3 = 0
-    maior4 = 0
-    maior5 = 0
-
-    if nome_gasto=="":
-        nome_gasto = "Gasto 0"
-        valor = 0
-
-    if nome_gasto!="":
-        if valor>maior:
-            lista_produtos_valorizados.insert(0,valor)
-            maior = valor
-        elif valor<=maior:
-            lista_produtos_valorizados.append(valor)
-    if len(lista_produtos_valorizados)==5:
-          maior1 = lista_produtos_valorizados[0]
-          maior2 = lista_produtos_valorizados[1]
-          maior3 = lista_produtos_valorizados[2]
-          maior4 = lista_produtos_valorizados[3]
-          maior5 = lista_produtos_valorizados[4]
-
-
+    nome_saida = ""
+    if tamanho_despesas ==0:
+        nome_saida = "Nada"
     return render_to_response("arquivos_para_sistema/pagina_apresentacao.html",
-        {"saida":nome_gasto, "gasto":valor},
+        {"tamanho1":tamanho_receitas, "saida":nome_saida},
                               context_instance=RequestContext(request))
 
 def receita_casa(request):
